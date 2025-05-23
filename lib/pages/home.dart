a61bec151b7b0c3cce390069f3e89a707962640d
vhalvarez <alvarez3197@gmail.com>
2025-05-23 13:22:03 -0400

    escuchar evento active-bands

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,12 +15,32 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Band> bands = [
-    Band(id: '1', name: 'Metallica', votes: 5),
-    Band(id: '2', name: 'Queen', votes: 1),
-    Band(id: '3', name: 'Bon jovi', votes: 4),
-    Band(id: '4', name: 'Linkin Park', votes: 3),
-    Band(id: '5', name: 'System Of A Down', votes: 5),
+    // Band(id: '1', name: 'Metallica', votes: 5),
+    // Band(id: '2', name: 'Queen', votes: 1),
+    // Band(id: '3', name: 'Bon jovi', votes: 4),
+    // Band(id: '4', name: 'Linkin Park', votes: 3),
+    // Band(id: '5', name: 'System Of A Down', votes: 5),
   ];
+
+  @override
+  void initState() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+
+    socketService.socket.on('active-bands', (payload) {
+      bands = (payload as List).map((band) => Band.fromMap(band)).toList();
+
+      setState(() {});
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.socket.off('active-bands');
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
